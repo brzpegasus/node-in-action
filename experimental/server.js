73bb4869ae9dd86b9317fb5f2206c5943dc41061
Estelle DeBlois <edeblois@gmail.com>
2013-04-15 14:06:38 -0400

    test redirection

--- experimental/server.js
+++ experimental/server.js
@@ -1,17 +1,17 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(req, res) {
+//http.createServer(function(req, res) {
     // Send plain text
     // res.writeHead(200, {'Content-Type': 'text/plain'});
     // res.end('Hello World');
 
     // Stream image data to the client
-    res.writeHead(200, {'Content-Type': 'image/png'});
-    fs.createReadStream('./resources/wow-drake.jpg').pipe(res);
+    //res.writeHead(200, {'Content-Type': 'image/png'});
+    //fs.createReadStream('./resources/wow-drake.jpg').pipe(res);
 
-}).listen(3000);
-console.log('Server running at http://localhost:3000/');
+//}).listen(3000);
+//console.log('Server running at http://localhost:3000/');
 
 // Alternative
 // var server = http.createServer();
@@ -20,3 +20,15 @@ console.log('Server running at http://localhost:3000/');
 //     res.end('Hello World');
 // });
 // server.listen(3000);
+
+var server = http.createServer(function(req, res) {
+    var url = 'http://google.com';
+    var body = '<p>Redirecting to <a href="' + url + '">' + url + '</a></p>';
+    res.setHeader('Location', url);
+    res.setHeader('Content-Length', body.length);
+    res.setHeader('Content-Type', 'text/html');
+    res.statusCode = 302;
+    res.end(body);
+});
+
+server.listen(3000, '127.0.0.1');
